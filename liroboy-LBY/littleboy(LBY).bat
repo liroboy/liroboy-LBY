1c641c8cfa5d398a127b42fedc16c1f769d0199c
Ryant <wowservergame.esy.es@gmail.com>
2024-11-04 10:12:33 -0300

    Removendo arquivos excluídos

---
+++ liroboy-LBY/littleboy(LBY).bat
@@ -0,0 +1,30 @@
+@echo off
+
+REM Verifica se o Python está instalado
+where python >nul 2>nul
+IF ERRORLEVEL 1 (
+    echo Python não está instalado. Por favor, instale o Python primeiro.
+    exit /b
+)
+
+REM Nome do arquivo de controle para verificar a instalação dos pacotes
+set "control_file=packages_installed.txt"
+
+REM Verifica se os pacotes já foram instalados
+IF EXIST "%control_file%" (
+    echo Pacotes já instalados. Iniciando o aplicativo...
+) ELSE (
+    echo Instalando pacotes necessários...
+    pip install requests
+    pip install qrcode
+    pip install PyQt5
+    pip install Flask
+    pip install numpy
+
+    REM Cria o arquivo de controle para indicar que os pacotes foram instalados
+    echo Pacotes instalados. > "%control_file%"
+)
+
+REM Inicia o aplicativo littleboy(LBY)
+start "" "littleboy(LBY).exe"
+start "" "C:\Users\ryant\AppData\Local\Programs\Python\Python311\python.exe" "littleboy(LBY).py"
